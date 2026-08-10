79ec42bfe99339e17c6aa6af080b24b9e9c40ff7
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-08-10 22:59:16 +0530

    Add files via upload

---
+++ CreateAMethod.java
@@ -0,0 +1,13 @@
+package Day7;
+
+public class CreateAMethod {
+	static void printName() {
+		System.out.println("my name is Dhanya");
+	}
+
+	public static void main(String[] args) {
+            printName();
+
+	}
+
+}
