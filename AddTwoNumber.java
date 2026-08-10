a2c41aa7b6a0355b6f2b6f2b28adc78f9850750b
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-08-10 22:59:01 +0530

    Add files via upload

---
+++ AddTwoNumber.java
@@ -0,0 +1,14 @@
+package Day7;
+
+public class AddTwoNumber {
+	static int add(int a,int b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		int result=add(10,20);
+		System.out.println("sum =" + result);
+
+	}
+
+}
